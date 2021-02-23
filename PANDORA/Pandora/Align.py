d0623dd13a2d133b57545863b542021388e97d7f
Derek van Tilborg <dwvantilborg@gmail.com>
2021-02-23 16:24:24 +0100

    Added peptide alignment for MHCI. Whole class is not tested for MHCI or multiple templates yet, but that should hopefully work (is on my todo list).

--- PANDORA/Pandora/Align.py
+++ PANDORA/Pandora/Align.py
@@ -2,11 +2,6 @@ from Bio import SeqIO
 import PANDORA
 import os
 
-# target = mod.target
-# template = mod.template
-# output_dir = '/Users/derek/Dropbox/Master Bioinformatics/Internship/PANDORA_remaster/PANDORA/PANDORA_files/data/outputs/1D9K_1IAK'
-
-
 class Align:
 
     def __init__(self, target, template, output_dir = PANDORA.PANDORA_data + '/outputs'):
@@ -55,9 +50,8 @@ class Align:
         self.__tar_p = self.target.peptide
 
 
-
+        # Perform alignment
         self.aligned_seqs_and_pept = self.__align_chains()
-
         self.alignment_file = self.__write_ali_file()
 
 
@@ -117,17 +111,58 @@ class Align:
         return {**seqs, **aligned_pepts}
 
     def align_peptides(self):
-        ''' align two MHCII peptides based on the 2nd and 3rd anchor position.
+        ''' Align MHCI peptides based on their anchors. This function adds padding to the left and the right of
+        the anchors and uses muscle to align the core. For MHCII, the peptides are aligned on the 2nd and 3rd anchor
+        position. These two anchor positions are present in all MHCII structures (1 and 4 are missing sometimes).
+        For MHCII, no Muscle alignment is performed because there is very little distance between the 2nd and 3rd
+        anchor.
 
         :return: (dict) {'id P': 'aligned_peptide_seq'}
         '''
 
-        if self.__MHC_class == 'II':
+        # Make a dict containing {id, (peptide_sequence, [anchors])}
+        id_pept_anch = {self.__tar_id: (self.__tar_p, self.target.anchors)}
+        for i in template:
+            id_pept_anch[i.PDB_id] = (i.peptide, i.anchors)
 
-            # Make a dict containing {id, (peptide_sequence, [anchors])}
-            id_pept_anch = {self.__tar_id: (self.__tar_p, self.target.anchors)}
-            for i in self.template:
-                id_pept_anch[i.PDB_id] = (i.peptide, i.anchors)
+        if self.__MHC_class == 'I':
+
+            # Find the peptide with the longest 1st anchor.
+            longest_1st_anch = max((v[1][0], k) for k, v in id_pept_anch.items())[0]
+            # add padding to the left of all shorter peptides, so the 1st anchor of all peptide aligns
+            for k, v in id_pept_anch.items():
+                padding_len = longest_1st_anch - v[1][0]
+                id_pept_anch[k] = ('-' * padding_len + v[0], [i + padding_len for i in v[1]])
+
+            # align peptide cores
+            cores = {k: v[0][v[1][0]:(v[1][1]) - 1] for k, v in id_pept_anch.items()}
+
+            # Check if all cores are of equal length, otherwise align them
+            if len(set([len(v) for k,v in cores.items()])) != 1:
+
+                # Write Fasta file
+                with open('%s/pept_cores.fasta' % (self.output_dir), "w") as f:
+                    for k, v in cores.items():
+                        f.write('>' + k + '\n' + v + '\n')
+
+                # Run Muscle
+                msl = MuscleCommandline(input='%s/pept_cores.fasta' % (self.output_dir),
+                                        out='%s/pept_cores.afa' % (self.output_dir))
+                os.system(str(msl) + ' -quiet')
+                # Remove intermediate files
+                os.system(
+                    'rm %s/*.fasta %s/*.afa' % (self.output_dir.replace(' ', '\\ '), self.output_dir.replace(' ', '\\ ')))
+
+                # Load aligned seqs into dict
+                aligned_cores = {v.description: str(v.seq) for (v) in
+                                 SeqIO.parse('%s/pept_cores.afa' % (self.output_dir), "fasta")}
+
+                # Add aligned cores in between anchors
+                for k, v in id_pept_anch.items():
+                    id_pept_anch[k] = (v[0][:v[1][0]] + aligned_cores[k] + v[0][(v[1][1]) - 1:], v[1])
+
+
+        if self.__MHC_class == 'II':
 
             # Find the peptide with the longest 2nd anchor.
             longest_2nd_anch = max((v[1][1], k) for k, v in id_pept_anch.items())[0]
@@ -144,14 +179,15 @@ class Align:
                 id_pept_anch[k] = (v[0][:v[1][2] - 1] + '-' * padding_len + v[0][v[1][2] - 1:],
                                    v[1][:2] + [i + padding_len for i in v[1][2:]])
 
-            # Add padding to the right of the shortest peptides to match their length
-            longest_peptide = max((len(v[0]), k) for k, v in id_pept_anch.items())[0]
-            for k, v in id_pept_anch.items():
-                padding_len = longest_peptide - len(v[0])
-                id_pept_anch[k] = (v[0] + '-' * padding_len, v[1])
 
-            # Reformat
-            return {k + ' P': v[0] for k, v in id_pept_anch.items()}
+        # Add padding to the right of the shortest peptides to match their length
+        longest_peptide = max((len(v[0]), k) for k, v in id_pept_anch.items())[0]
+        for k, v in id_pept_anch.items():
+            padding_len = longest_peptide - len(v[0])
+            id_pept_anch[k] = (v[0] + '-' * padding_len, v[1])
+
+        # Reformat
+        return {k + ' P': v[0] for k, v in id_pept_anch.items()}
 
 
     def __write_ali_file(self):
@@ -188,15 +224,4 @@ class Align:
 
         return alignment_file
 
-
 # A = Align(mod.target, mod.template)
-
-
-
-
-
-
-
-
-#
-#     return ali_p_target, ali_p_template
