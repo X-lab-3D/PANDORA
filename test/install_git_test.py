b534f2a7efafe160f9751ff19cfd3ecdfa246dbf
DarioMarzella <dario.marzella@outlook.it>
2022-09-05 18:01:08 +0000

    test

--- test/install_git_test.py
+++ test/install_git_test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 os.system('mkdir ./PANDORA_files')
 os.system('mkdir ./PANDORA_files/data')
@@ -15,6 +16,18 @@ os.system('mkdir ./test/test_data/PDBs/Bad/pMHCI')
 os.system('mkdir ./test/test_data/PDBs/Bad/pMHCII')
 os.system('mkdir ./test/test_data/csv_pkl_files')
 
+modeller_config_file = '/usr/share/miniconda/envs/test/lib/modeller-10.2/modlib/modeller/config.py'
+modeller_license_key = sys.arg[1]
+
+modeller_config = []
+with open(modeller_config_file, 'r') as f:
+    for line in f:
+        modeller_config.append(line)
+with open(modeller_config_file, 'w') as f:
+    print("Writing MODELLER config file")
+    for line in f:
+        f.write(line.replace("XXXX", modeller_license_key))
+        print(line.replace("XXXX", modeller_license_key))
 
 
 # netMHCII Pan install
