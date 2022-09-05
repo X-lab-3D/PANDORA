41f4c050353b3af41d614599eb12ee5cacfebffb
DarioMarzella <dario.marzella@outlook.it>
2022-09-05 19:36:35 +0000

    test os.environ instead of argparse

--- test/set_modeller_license.py
+++ test/set_modeller_license.py
@@ -1,13 +1,15 @@
 import argparse
+import os
 
-arg_parser = argparse.ArgumentParser(description="Sets modeller license key")
-arg_parser.add_argument(
-    "--license","-l",
-    help="MODELLER license key code",
-    required=True
-)
-a = arg_parser.parse_args()
+# arg_parser = argparse.ArgumentParser(description="Sets modeller license key")
+# arg_parser.add_argument(
+#     "--license","-l",
+#     help="MODELLER license key code",
+#     required=True
+# )
+# a = arg_parser.parse_args()
 
+license = os.environ['KEY_MODELLER']
 modeller_config_file = '/usr/share/miniconda/lib/modeller-10.2/modlib/modeller/config.py'
 
 modeller_config = []
@@ -18,5 +20,5 @@ with open(modeller_config_file, 'r') as f:
 with open(modeller_config_file, 'w') as f:
     print("Writing MODELLER config file")
     for line in modeller_config:
-        f.write(line.replace("XXXX", a.license))
-        print(line.replace("XXXX", a.license))
+        f.write(line.replace("XXXX", license))
+        print(line.replace("XXXX", license))
