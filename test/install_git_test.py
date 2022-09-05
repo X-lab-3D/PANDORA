e73f773ef4b65bb48f1c1e67a89e9301449fc387
DarioMarzella <dario.marzella@outlook.it>
2022-09-05 18:09:17 +0000

    Define modeller key without alias

--- test/install_git_test.py
+++ test/install_git_test.py
@@ -16,18 +16,18 @@ os.system('mkdir ./test/test_data/PDBs/Bad/pMHCI')
 os.system('mkdir ./test/test_data/PDBs/Bad/pMHCII')
 os.system('mkdir ./test/test_data/csv_pkl_files')
 
-modeller_config_file = '/usr/share/miniconda/envs/test/lib/modeller-10.2/modlib/modeller/config.py'
-modeller_license_key = sys.argv[1]
+# modeller_config_file = '/usr/share/miniconda/envs/test/lib/modeller-10.2/modlib/modeller/config.py'
+# modeller_license_key = sys.argv[1]
 
-modeller_config = []
-with open(modeller_config_file, 'r') as f:
-    for line in f:
-        modeller_config.append(line)
-with open(modeller_config_file, 'w') as f:
-    print("Writing MODELLER config file")
-    for line in f:
-        f.write(line.replace("XXXX", modeller_license_key))
-        print(line.replace("XXXX", modeller_license_key))
+# modeller_config = []
+# with open(modeller_config_file, 'r') as f:
+#     for line in f:
+#         modeller_config.append(line)
+# with open(modeller_config_file, 'w') as f:
+#     print("Writing MODELLER config file")
+#     for line in f:
+#         f.write(line.replace("XXXX", modeller_license_key))
+#         print(line.replace("XXXX", modeller_license_key))
 
 
 # netMHCII Pan install
