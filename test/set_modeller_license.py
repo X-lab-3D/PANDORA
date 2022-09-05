9176e7d32c074caf41878ce919d7c91a83cc840f
DarioMarzella <dario.marzella@outlook.it>
2022-09-05 18:57:28 +0000

    add set_modeller_license.py

--- test/set_modeller_license.py
+++ test/set_modeller_license.py
@@ -1,7 +1,14 @@
-import sys
+import argparse
+
+arg_parser = argparse.ArgumentParser(description="Sets modeller license key")
+arg_parser.add_argument(
+    "--license","-l",
+    help="MODELLER license key code",
+    required=True
+)
+a = arg_parser.parse_args()
 
 modeller_config_file = '/usr/share/miniconda/envs/test/lib/modeller-10.2/modlib/modeller/config.py'
-modeller_license_key = sys.argv[1]
 
 modeller_config = []
 with open(modeller_config_file, 'r') as f:
@@ -10,5 +17,5 @@ with open(modeller_config_file, 'r') as f:
 with open(modeller_config_file, 'w') as f:
     print("Writing MODELLER config file")
     for line in f:
-        f.write(line.replace("XXXX", modeller_license_key))
-        print(line.replace("XXXX", modeller_license_key))
+        f.write(line.replace("XXXX", a.license))
+        print(line.replace("XXXX", a.license))
