7637bb2c8009f465b8b0dd11fb3f87eab1044b58
DarioMarzella <dario.marzella@outlook.it>
2022-11-15 21:02:27 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -18,65 +18,6 @@ with open(os.path.join(here, 'PANDORA', '__version__.py')) as f:
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        develop.run(self)
-        post_install_jobs()
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        post_install_jobs()
-
-setup(
-    name='PANDORA',
-    version=version['__version__'],
-    description='Model peptide-MHC complexes using anchor distance restrains in MODELLER',
-    long_description=readme + '\n\n',
-    long_description_content_type='text/markdown',
-    author='Dario Marzella, Farzaneh Parizi, Li Xue',
-    url='https://github.com/X-lab-3D/PANDORA/tree/master',
-    project_urls={
-        'Source Code': 'https://github.com/X-lab-3D/PANDORA/tree/master',
-        'Issue tracker': 'https://github.com/X-lab-3D/PANDORA/issues'
-    },
-    packages=find_packages(),
-    include_package_data=True,
-    license="Apache Software License 2.0",
-    keywords='PANDORA',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-
-    install_requires=[
-       'Biopython',
-       'pdb2sql',
-       'joblib'
-       ],
-
-    extras_require={
-        'doc': ['recommonmark', 'sphinx', 'sphinx_rtd_theme'],
-        'test': ['pytest', 'pytest-runner', 'pytest-cov',
-                 'coverage', 'coveralls', 'pycodestyle']
-    },
-    
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
-)
-
 def post_install_jobs():
     user_folder_path = Path(__file__).parents[0]
 
@@ -122,3 +63,62 @@ def post_install_jobs():
     except Exception as e:
         print(f'WARNING: received error while installing database: {e}')
         print('To be able to use PANDORA you will have to generate a new database. Please follow the instructions in the README.')
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        post_install_jobs()
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        post_install_jobs()
+
+
+setup(
+    name='PANDORA',
+    version=version['__version__'],
+    description='Model peptide-MHC complexes using anchor distance restrains in MODELLER',
+    long_description=readme + '\n\n',
+    long_description_content_type='text/markdown',
+    author='Dario Marzella, Farzaneh Parizi, Li Xue',
+    url='https://github.com/X-lab-3D/PANDORA/tree/master',
+    project_urls={
+        'Source Code': 'https://github.com/X-lab-3D/PANDORA/tree/master',
+        'Issue tracker': 'https://github.com/X-lab-3D/PANDORA/issues'
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    license="Apache Software License 2.0",
+    keywords='PANDORA',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+
+    install_requires=[
+       'Biopython',
+       'pdb2sql',
+       'joblib'
+       ],
+
+    extras_require={
+        'doc': ['recommonmark', 'sphinx', 'sphinx_rtd_theme'],
+        'test': ['pytest', 'pytest-runner', 'pytest-cov',
+                 'coverage', 'coveralls', 'pycodestyle']
+    },
+    
+    cmdclass={
+        'develop': PostInstallCommand,
+        'install': PostInstallCommand,
+    },
+)
