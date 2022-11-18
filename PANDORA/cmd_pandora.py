0a54df7be9e1c8bb2a93bf5c5644888ffd2b5abf
DarioMarzella <dario.marzella@outlook.it>
2022-11-18 17:04:12 +0000

    Add four basic command line tools.

--- PANDORA/cmd_pandora.py
+++ PANDORA/cmd_pandora.py
@@ -3,9 +3,10 @@ import argparse
 from PANDORA import Target
 from PANDORA import Pandora
 from PANDORA import Database
+from PANDORA import Wrapper
 
 def cmd_install_database():
-    """Command line tool to run install_database.
+    """Command line tool to download the database from zotero
     """    
     parser = argparse.ArgumentParser(
     prog="PANDORA",
@@ -21,7 +22,38 @@ def cmd_install_database():
 
     Database.install_database(db_path=args.destination_path)
 
-def run_pandora():
+def cmd_create_database():
+#if __name__=='__main__':
+    """Command line tool to generate a new database crom scratch
+    """    
+    parser = argparse.ArgumentParser(
+    prog="PANDORA",
+    description="Generate a new PANDORA database",
+    )
+
+    parser.add_argument(
+        '-d','--destination-path', default='~/PANDORA_databases/default',
+        help='Full path to the database destination'
+    )
+
+    parser.add_argument(
+        '-c','--num-cores', default=1, type=int,
+        help='Number of CPU cores to use in parallel to generate the database\
+            using multiple cores is highly recommended as it drastically reduces\
+            the database generation time.',
+    )
+
+    args = parser.parse_args()
+    ## import requested modules
+    from PANDORA import Database
+
+    ## A. Create local Database
+    db = Database.Database()
+    db.construct_database(n_jobs=args.num_cores)
+
+
+def cmd_run_pandora():
+#if __name__=='__main__':
     """Command line tool to run one pandora case
     """
     parser = argparse.ArgumentParser(
@@ -31,35 +63,40 @@ def run_pandora():
 
     parser.add_argument(
         '-p','--peptide', required=True, type=str,
-        help='One-letter sequence of the target peptide.'
+        help='One-letter sequence of the target peptide.',
     )
 
     parser.add_argument(
         '-a','--allele', required=True, type=str,
-        help='Name of the target MHC allele'
+        help='Name of the target MHC allele',
     )
     
     parser.add_argument(
-        '-c','--mhc-class', required=True, type=str,
-        help='MHC class', choices=['I','II']
+        '-m','--mhc-class', required=True, type=str,
+        help='MHC class', choices=['I','II'],
     )
 
     parser.add_argument(
         '-i','--id', 
-        help='ID of the target case.\
-             If not provided, it will default to peptide_allele'
+        help='ID of the target case, used to name the output folder.\
+             If not provided, it will default to peptide_allele',
     )
 
     parser.add_argument(
         '-k','--anchors', 
         help='Peptide anchor positions.\
             To be provided as series of integers separated by comma only.\
-            Example: 2,9 for pMHC-I or 4,7,9,12 for pMHC-II'
+            Example: 2,9 for pMHC-I or 4,7,9,12 for pMHC-II',
+    )
+
+    parser.add_argument(
+        '-l','--loop-models', default=20, type=int,
+        help='Number of loop models to produce',
     )
 
     parser.add_argument(
         '-o','--output-path', default='./',
-        help='Output folder.'
+        help='Output folder.',
     )
 
     args = parser.parse_args()
@@ -68,8 +105,11 @@ def run_pandora():
     if not args.id:
         args.id = f'{args.peptide}_{args.allele}'
     
+    if not args.anchors:
+        args.anchors = []
+    else:
+        args.anchors = [int(x) for x in args.anchors.split(',')]
     
-
     ## A. Load local Database
     db = Database.load()
 
@@ -77,9 +117,99 @@ def run_pandora():
     target = Target(id = args.id,
         allele_type = args.allele,
         peptide = args.peptide,
-        anchors = [2,9])
+        anchors = args.anchors)
 
     ## C. Perform modelling
     case = Pandora.Pandora(target, db)
-    case.model()
+    case.model(n_loop_models=args.loop_models)
 
+def cmd_run_wrapper():
+    """Command line tool to run the Wrapper module
+        over one tsv/csv file.
+    """
+    parser = argparse.ArgumentParser(
+    prog="PANDORA",
+    description="Run one pMHC modelling case",
+    )
+
+    parser.add_argument(
+        '-f','--input-file', required=True, type=str,
+        help='Input .tsv or .csv file',
+    )
+
+    parser.add_argument(
+        '-m','--mhc-class', required=True, type=str,
+        help='MHC class', choices=['I','II'],
+    )
+
+    parser.add_argument(
+        '-d','--delimiter', type=str, default=',',
+        help='Input file delimiter', choices=['tab',','],
+    )    
+
+    parser.add_argument(
+        '-H','--header', store_true=True, required=True,
+        help='Wether there is or not a header. If True, the first line\
+            of the input file will be ignored for the modellings.',
+    )    
+
+    parser.add_argument(
+        '-i','--id', default=False,
+        help='ID of the wrapper, used to name the output folder.Should be alphanumeric only.\
+                If not, non-alphanumeric characters will be replaced with dashes.\
+                If False, it will be randomly generated. Defaults to False.',
+    )
+
+    parser.add_argument(
+        '-n','--num-cores', default=1, type=int,
+        help='Number of CPU cores to use to model the targets in parallel. Default to 1.',
+    )
+
+    parser.add_argument(
+        '-t','--targets-id-column', type=int,
+        help="0-index of the column containing each target's ID. ",
+    )
+
+    parser.add_argument(
+        '-p','--peptides-column', required=True, type=int,
+        help='0-index of the column containing one-letter target peptides sequences.',
+    )
+
+    parser.add_argument(
+        '-a','--allele-name-column', required=True, type=int,
+        help='0-index of the column containing MHC allele names, separated by semicolon (;)', 
+    )
+
+    parser.add_argument(
+        '-k','--anchors-column', type=str,
+        help='Peptide anchor positions.\
+            To be provided as series of integers separated by semicolon only.\
+            Example: 2;9 for pMHC-I or 4;7;9;12 for pMHC-II',
+    )
+
+    parser.add_argument(
+        '-l','--loop-models', default=20, type=int,
+        help='Number of loop models to produce',
+    )
+
+    parser.add_argument(
+        '-o','--output-path', default='./',
+        help='Output folder.',
+    )
+
+    args = parser.parse_args()
+
+    ## A. Load local database
+    db = Database.load()
+
+    if args.delimiter=='tab':
+        args.delimiter='\t'
+
+    ## B. Create the wrapper object. It will also run the modelling for each case.
+    wrap =  Wrapper.Wrapper(data_file=args.input_file, database=db, 
+        num_cores=args.num_cores, wrapper_id=args.id, header=args.header,
+        MHC_class=args.mhc_class, delimiter=args.delimiter,
+        IDs_col=args.targets_id_column, peptides_col=args.peptides_column,
+        allele_name_col=args.allele_name_column, anchors_col=args.anchors_column,
+        n_loop_models=args.loop_models, collective_output_dir=args.output_path,
+        )
